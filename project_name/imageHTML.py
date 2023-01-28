e797dd4f96b2bd139a027553dc4cb52b611eed4c
Liam Sy <liamrs222@gmail.com>
2023-01-28 13:29:33 -0500

    added downloading images

--- project_name/imageHTML.py
+++ project_name/imageHTML.py
@@ -1,11 +1,27 @@
+import os
 import requests
+import urllib.request
 from bs4 import BeautifulSoup 
-    
-def getdata(url): 
+
+# Receives a url and returns it as text
+def getData(url): 
     r = requests.get(url) 
     return r.text 
-    
-htmldata = getdata("https://cloud.google.com/") 
+
+# Given an image URL, downloads the image into a folder
+def downloadImg(image_url, folder):
+    filename = folder + "/" + image_url.split("/")[-1] 
+
+    r = requests.get(image_url, stream = True)
+    if r.ok:
+        urllib.request.urlretrieve(image_url, filename)
+        print("Download successful ", filename)
+    else:
+        print("Download failed")
+    os.remove(filename)
+   
+htmldata = getData("https://www.geeksforgeeks.org/") 
 soup = BeautifulSoup(htmldata, 'html.parser') 
-for item in soup.find_all('img'):
-    print(item['src'])
+for image in soup.find_all('img'):
+    print(image['src'])
+    downloadImg(image['src'], "images")
